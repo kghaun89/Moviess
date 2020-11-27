2882a48558b8093ebe0ea66a08ac43c0469806f9
kghaun89 <56031109+kghaun89@users.noreply.github.com>
2020-11-27 11:20:26 -0600

    Add files via upload

---
+++ ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Moviess.Models;
+
+namespace Moviess.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+
+        public Customer customer { get; set; }
+    }
+}
---
+++ ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Moviess.Models;
+
+namespace Moviess.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre> Genres { get; set;  }
+
+        public Movie movie { get; set; }
+    }
+}
